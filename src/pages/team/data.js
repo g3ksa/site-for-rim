014cf5b6f0815c4cce6ff36f9f86812376c9f5c0
g3ksa <temka.kuzmin14@gmail.com>
2022-05-31 01:27:04 +0500

    Add links to team's page

--- src/pages/team/data.js
+++ src/pages/team/data.js
@@ -7,6 +7,9 @@ export const teamData = [
       name: 'Артём',
       surname: 'Кузьмин',
       avatar: Artem,
+      InstLink: 'https://www.instagram.com/nit3bo1/',
+      VKLink: 'https://vk.com/nit3bo1',
+      TGLink: 't-do.ru/nit3bo1',
       description:
          'Руководитель команды. Был ответственен за создании диаграмм и сайта, написании текста для выступления и нахождении информации',
    },
@@ -14,6 +17,9 @@ export const teamData = [
       name: 'Павел',
       surname: 'Неткачев',
       avatar: Pasha,
+      InstLink: 'https://www.instagram.com/pavel_nt4v/',
+      VKLink: 'https://vk.com/b1ondyyy',
+      TGLink: 't-do.ru/B1ondy',
       description:
          'Верстальщик. Был ответственен за создание презентации и нахождении информации',
    },
@@ -21,6 +27,9 @@ export const teamData = [
       name: 'Ростислав',
       surname: 'Пайсов',
       avatar: Rostik,
+      InstLink: 'https://www.instagram.com/bibik_15/',
+      VKLink: 'https://vk.com/rostikpass',
+      TGLink: 't-do.ru/bibik_15',
       description:
          'Исследователь свежей информации. Был ответственен за анализ данных и помощь в создании презентации',
    },
--- src/pages/team/index.jsx
+++ src/pages/team/index.jsx
@@ -11,6 +11,9 @@ import styles from './team.module.scss';
 import { Container } from '../../components/container';
 import { Block } from '../../components/block';
 import { Title } from '../../components/title';
+import { IoLogoInstagram } from 'react-icons/io5';
+import { IoLogoVk } from 'react-icons/io5';
+import { FaTelegram } from 'react-icons/fa';
 
 const Team = () => {
    const members = teamData.map((member) => (
@@ -18,13 +21,29 @@ const Team = () => {
          <div className={styles.left}>
             <div className={styles.avatar}>
                <img src={member.avatar} alt='' />
-               <div className={styles.fullname}>
-                  {`${member.surname} ${member.name}`}
-               </div>
             </div>
          </div>
          <div className={styles.right}>
+            <div className={styles.fullname}>
+               {`${member.surname} ${member.name}`}
+            </div>
             <div className={styles.description}>{member.description}</div>
+            <div className={styles.links}>
+               <a href={member.TGLink} className={styles.link} target='_blank'>
+                  <FaTelegram size={30} />
+               </a>
+               <a href={member.VKLink} className={styles.link} target='_blank'>
+                  <IoLogoVk size={30} />
+               </a>
+
+               <a
+                  href={member.InstLink}
+                  className={styles.link}
+                  target='_blank'
+               >
+                  <IoLogoInstagram size={30} />
+               </a>
+            </div>
          </div>
       </SwiperSlide>
    ));
