1f8cdd0aebdf7ff19055294b97a946a62d140a98
g3ksa <temka.kuzmin14@gmail.com>
2022-05-31 02:46:14 +0500

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,19 @@ import { Stats } from './pages/stats';
 import { Team } from './pages/team';
 import { Layout } from './components/Layout';
 import { Tasks } from './pages/tasks';
-import { AnimatePresence } from 'framer-motion';
 
 const App = () => {
    return (
       <>
-         <AnimatePresence exitBeforeEnter>
-            <Routes>
-               <Route path='/' element={<Layout />}>
-                  <Route index element={<MainBlock />} />
-                  <Route path='sources' element={<Sources />} />
-                  <Route path='stats' element={<Stats />} />
-                  <Route path='team' element={<Team />} />
-                  <Route path='tasks' element={<Tasks />} />
-               </Route>
-            </Routes>
-         </AnimatePresence>
+         <Routes>
+            <Route path='/' element={<Layout />}>
+               <Route index element={<MainBlock />} />
+               <Route path='sources' element={<Sources />} />
+               <Route path='stats' element={<Stats />} />
+               <Route path='team' element={<Team />} />
+               <Route path='tasks' element={<Tasks />} />
+            </Route>
+         </Routes>
       </>
    );
 };
